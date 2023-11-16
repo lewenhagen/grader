5613c4575ba66b22123b3d8658e9bb3c48063ce2
Kenneth Lewenhagen <lewenhagen@live.com>
2023-11-16 12:39:15 +0100

    restructure

--- grader.bash
+++ grader.bash
@@ -4,7 +4,7 @@ AUTH="Authorization: Bearer 12133~iiDecoHsYRNR8XzVbNyYaZ1OnZFm3DkTQ2p960WbGptGIc
 
 BASE_URL="https://bth.instructure.com"
 
-
+COURSEID="5224"
 
 LENGTH=1
 PAGE=1
@@ -14,17 +14,18 @@ COUNTER=0
 function present
 {
   echo ${TEACHERS[2]}
-  awk -f fix.awk $GRADEBOOK
-  #sort $GRADEBOOK | uniq -c
-  
+  awk -f fix.awk $GRADEBOOK 
+
+  exit 0
 }
 
-if [[ "$1" = "new" ]]; then
+function getnew
+{
   echo "Getting new data..."
   > $GRADEBOOK
   while [[ true ]]
   do
-    GRADEBOOK_URL="$BASE_URL/api/v1/courses/5224/gradebook_history/feed?page=$PAGE&per_page=100"
+    GRADEBOOK_URL="$BASE_URL/api/v1/courses/$COURSEID/gradebook_history/feed?page=$PAGE&per_page=100"
     GRADEBOOK_HOLDER=$(curl -H "$AUTH" -s "$GRADEBOOK_URL")
     LENGTH=$(echo $GRADEBOOK_HOLDER | jq length)
     [[ $LENGTH == 0 ]] && exit 0
@@ -34,8 +35,12 @@ if [[ "$1" = "new" ]]; then
     ((PAGE++))
   done
 
-elif [[ "$1" = "print" ]]; then
-  present
-else
-  printf "%s\n%s\n%s\n" "Commands:" "'print' (Prints current stats)" "'new' (Fetches new data from Canvas)"
-fi
+  exit 0
+}
+
+[[ "$1" = "new" ]] && getnew
+[[ "$1" = "print" ]] && present
+  
+printf "%s\n%s\n%s\n" "Commands:" "'print' (Prints current stats)" "'new' (Fetches new data from Canvas)"
+
+exit 1
